ca6c1d9cef110c5f8bba655284e6f99f7881b66e
Rienk Koenders <rienk.koenders@student.uva.nl>
2018-01-18 22:46:22 +0100

    Jinja2 & bootstrap verwerkt in de applicatie

--- Source/app.py
+++ Source/app.py
@@ -2,11 +2,23 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from flask_session import Session
 import sqlite3
 
-
-connection = sqlite3.connect("data.db", check_same_thread=False)
+# Initialize the application.
 app = Flask(__name__)
 
+# Makes a connection with the database.
+connection = sqlite3.connect("data.db", check_same_thread=False)
+
+# Put application into debug mode, to disbale caching.
 app.debug = True
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 
 @app.route("/")
 def index():
@@ -17,8 +29,7 @@ def index():
     db.execute("INSERT INTO users (username, password) VALUES(?, ?)", (var1, var2))
     connection.commit()
 
-    test = 'testsss2'
-    return render_template("index.html", test=test)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
