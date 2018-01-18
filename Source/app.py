2ffcb06bd647b30d04bb4f377234714d5eba9c7d
Rienk Koenders <rienk.koenders@student.uva.nl>
2018-01-18 16:03:33 +0100

    Caching uitgeschakelt bij flask framework, door debug mode in te schakelen.

--- Source/app.py
+++ Source/app.py
@@ -1,18 +1,14 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-# from flask_session import Session
+from flask_session import Session
 app = Flask(__name__)
 
-# if app.config["DEBUG"]:
-#     @app.after_request
-#     def after_request(response):
-#         response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#         response.headers["Expires"] = 0
-#         response.headers["Pragma"] = "no-cache"
-#         return response
+app.debug = True
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    test = 'test2'
+    return render_template("index.html", test=test)
 
 if __name__ == "__main__":
     app.run()
