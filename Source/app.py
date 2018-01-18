c4a916a5b6588111cfd46c29af83f35ed4f8bb78
Rienk Koenders <rienk.koenders@student.uva.nl>
2018-01-18 18:54:20 +0100

    Offline database toegevoegd, zonder cs50 troep met sqlite3

--- Source/app.py
+++ Source/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
+import sqlite3
+
+
+connection = sqlite3.connect("data.db", check_same_thread=False)
 app = Flask(__name__)
 
 app.debug = True
-app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    test = 'test2'
+
+    db = connection.cursor()
+    var1 = 'y'
+    var2 = 'x'
+    db.execute("INSERT INTO users (username, password) VALUES(?, ?)", (var1, var2))
+    connection.commit()
+
+    test = 'testsss2'
     return render_template("index.html", test=test)
 
 if __name__ == "__main__":
